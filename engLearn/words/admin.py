c620d9dcd088c3114b02e1634a90db0d3ee4c8d0
vladiuse <vladiuse@gmail.com>
2024-07-23 21:19:56 +0300

    add words files

--- engLearn/words/admin.py
+++ engLearn/words/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Word
 
-# Register your models here.
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ['number_in_dict', 'eng', 'ru']
+    search_fields = ['eng', 'ru']
+    list_display_links = ['eng', 'ru']
+
+
+admin.site.register(Word, WordAdmin)
