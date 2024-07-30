8c3e78b8415d2ca336a13354e4db989175b07355
vladiuse <vladiuse@gmail.com>
2024-07-30 19:12:01 +0300

    add Voc models

--- engLearn/vocabulary/models.py
+++ engLearn/vocabulary/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from words.models import Word
 
-# Create your models here.
+
+
+class UserWord(models.Model):
+    LEARNED = 'learned'
+    NOT_LEARNED = 'not_learned'
+    STATUSES = (
+        (LEARNED,'Выучено'),
+        (NOT_LEARNED,'Не выучено'),
+
+    )
+    word = models.ForeignKey(
+        to=Word,
+        on_delete=models.CASCADE,
+    )
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        max_length=12,
+        choices=STATUSES,
+        default=NOT_LEARNED,
+    )
+    wrong_answer_count = models.PositiveIntegerField(
+        blank=True,
+        default=0,
+    )
+    correct_answer_count = models.PositiveIntegerField(
+        blank=True,
+        default=0,
+    )
