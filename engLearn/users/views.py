878936baa26e0017431e089e8f83d26744b518b7
vladiuse <vladiuse@gmail.com>
2024-07-31 20:28:21 +0300

    add user app

---
+++ engLearn/users/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+
+class RegisterView(TemplateView):
+
+    template_name = 'registration/sign_up.html'
+
+    def post(self, request, *args, **kwargs):
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            login(request,user)
+            return HttpResponseRedirect('/')
+        else:
+            print(repr(form.errors))
+            content = {
+                'form': form
+            }
+            return render(request, self.template_name, content)
+
+
