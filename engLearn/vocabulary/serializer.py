99dbe1ac194bc908dcd42d5044de26fc979affdc
vladiuse <vladiuse@gmail.com>
2024-07-31 18:46:06 +0300

    add UserWord serializer

---
+++ engLearn/vocabulary/serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import UserWord
+from rest_framework.validators import UniqueTogetherValidator
+
+class UserWordSerializer(serializers.ModelSerializer):
+
+    def to_internal_value(self, data):
+        data['owner'] = self.context['request'].user.pk
+        ret = super().to_internal_value(data)
+        return ret
+
+    class Meta:
+        model = UserWord
+        fields ='__all__'
+        read_only_fields = ('wrong_answer_count', 'correct_answer_count', )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=UserWord.objects.all(),
+                fields=['owner', 'word']
+            )
+        ]
